e485bb019598d7d29d58b11c13f4e163a56cca79
Daniel Bae <danibae91@icloud.com>
2021-05-20 19:35:45 -0700

    create AddPetForm

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional 
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets"""
+
+    # Basic Fields 
+    name = StringField('Pet Name', validators=[InputRequired()])
+    species = SelectField('Species', choices=[('cat', 'Cat'), ('dog','Dog'), ('porcupine', 'Porcupine')])
+    photo_url = StringField('Image URL', validators=[Optional(), URL()])
+    age = IntegerField('Age', validators=[Optional(), NumberRange(min=0, max=30)]) 
+    notes = TextAreaField('Notes', validators=[Optional(), Length(min=15)])
