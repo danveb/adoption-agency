a14a2a07d98761f8ca7a38845c4fdf4c4c8ad0a1
Daniel Bae <danibae91@icloud.com>
2021-05-20 19:34:19 -0700

    create new route for adding pets

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet 
+from forms import AddPetForm
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def home_page():
+    pets = Pet.query.all()
+    return render_template('home.html', pets=pets)
+
+@app.route('/add', methods=["GET", "POST"]) 
+def add_pet():
+    form = AddPetForm() # make new Form Object 
+
+    # validate CSRF Token on submit 
+    if form.validate_on_submit():
+        # form data 
+        name = form.name.data
+        species = form.name.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data 
+
+        # new_pet 
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+
+        # db.session
+        db.session.add(new_pet)
+        db.session.commit() 
+
+        # redirect 
+        return redirect('/')
+    else: 
+        # render form 
+        return render_template('add_pet_form.html', form=form)
